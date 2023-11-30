a0af0b11aaa23e7f8b63c68708d272f65d27cae3
Elena <Xelen1974@mail.ru>
2023-11-30 15:56:50 +0600

    commit Task05

---
+++ Task05/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "meri") //не читает русские букы, только английские
+{
+    Console.WriteLine("Ура! Это же MERI!");
+}
+
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
