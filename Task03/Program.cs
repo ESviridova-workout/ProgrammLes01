b9e1b191bdcb12e75b8034407d76839caa38db31
Elena <Xelen1974@mail.ru>
2023-11-30 14:55:04 +0600

    commit Task03_ Random

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -1,4 +1,6 @@
-﻿int numbeA = 8;
-int numbeB = 51;
-int result = numbeA+numbeB;
+﻿int numberA = new Random().Next(1, 10); //т.е. это полуинтервал: 1 2 3 4 5 6 7 8 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result = numberA+numberB;
 Console.WriteLine(result);
